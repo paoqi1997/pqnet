694515e811d944b36c7a43c5bf44337ef6ec1bd0
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2019-02-12 23:56:44 +0800

    asynclog_test

---
+++ src/util/tests/asynclog_test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include "../signal.h"
+#include "../thread.h"
+#include "../threadpool.h"
+
+static bool done = false;
+
+void sighandler(int signo) {
+    done = !done;
+}
+
+void* func(void *arg) {
+    auto self = static_cast<pqnet::Thread*>(arg);
+    auto pool = self->getPool();
+    ALOG_INFO(pool->al, "Hello pqnet!");
+}
+
+int main()
+{
+    pqnet::ThreadPool pool(2, func);
+    pool.start();
+    pqnet::Signal sig(sighandler);
+    sig.addSignal(SIGINT);
+    sig.addSignal(SIGTERM);
+    sig.waitSig();
+    for ( ; ; ) {
+        if (done && pool.isEmpty()) {
+            pool.stop();
+            break;
+        }
+    }
+    std::cout << "Bye!" << std::endl;
+    return 0;
+}
