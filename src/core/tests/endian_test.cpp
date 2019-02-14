fea2b67c4fe1cd50338bb18a35c02dbf824de48f
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2019-02-14 22:13:10 +0800

    fix bug

--- src/core/tests/endian_test.cpp
+++ src/core/tests/endian_test.cpp
@@ -9,11 +9,11 @@ int main()
     std::uint16_t h16val = 8088;
     std::uint32_t h32val = 12488;
 
-    std::uint16_t n16val = pqnet::host2Net16(h16val);
-    std::uint32_t n32val = pqnet::host2Net32(h32val);
+    std::uint16_t n16val = pqnet::h2n16(h16val);
+    std::uint32_t n32val = pqnet::h2n32(h32val);
     
-    std::uint16_t _h16val = pqnet::net2Host16(n16val);
-    std::uint32_t _h32val = pqnet::net2Host32(n32val);
+    std::uint16_t _h16val = pqnet::n2h16(n16val);
+    std::uint32_t _h32val = pqnet::n2h32(n32val);
 
     std::cout << h16val << ' ' << n16val << ' ' << _h16val << std::endl;
     std::cout << h32val << ' ' << n32val << ' ' << _h32val << std::endl;
