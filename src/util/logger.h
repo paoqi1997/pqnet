870f69d5e51b0e7e434c03225eaa67cfaa8ca4e7
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2019-02-14 12:40:00 +0800

    adjust logger

--- src/util/logger.h
+++ src/util/logger.h
@@ -10,19 +10,19 @@ namespace pqnet
 {
 
 // To Terminal
-#define TRACE(msg)                                                                                \
-{                                                                                                 \
-    fprintf(stdout, "[Trace] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg); \
+#define TRACE(msg)                                                                                  \
+{                                                                                                   \
+    fprintf(stdout, "[Trace] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg);   \
 }
 
-#define DEBUG(msg)                                                                                \
-{                                                                                                 \
-    fprintf(stdout, "[Debug] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg); \
+#define DEBUG(msg)                                                                                  \
+{                                                                                                   \
+    fprintf(stdout, "[Debug] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg);   \
 }
 
-#define INFO(msg)                                                                                \
-{                                                                                                \
-    fprintf(stdout, "[Info] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg); \
+#define INFO(msg)                                                                                   \
+{                                                                                                   \
+    fprintf(stdout, "[Info] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg);    \
 }
 
 #define WARNING(msg)                                                                                \
@@ -30,30 +30,30 @@ namespace pqnet
     fprintf(stdout, "[Warning] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg); \
 }
 
-#define ERROR(msg)                                                                                \
-{                                                                                                 \
-    fprintf(stderr, "[Error] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg); \
+#define ERROR(msg)                                                                                  \
+{                                                                                                   \
+    fprintf(stderr, "[Error] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg);   \
 }
 
-#define FATAL(msg)                                                                                \
-{                                                                                                 \
-    fprintf(stderr, "[Fatal] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg); \
+#define FATAL(msg)                                                                                  \
+{                                                                                                   \
+    fprintf(stderr, "[Fatal] %s %s:%d: %s\n", pqnet::now().toDefault(), __FILE__, __LINE__, msg);   \
 }
 
 // To Log File
-#define LOG_TRACE(msg)                                                \
-{                                                                     \
-    pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::TRACE).log(msg); \
+#define LOG_TRACE(msg)                                                  \
+{                                                                       \
+    pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::TRACE).log(msg);   \
 }
 
-#define LOG_DEBUG(msg)                                                \
-{                                                                     \
-    pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::DEBUG).log(msg); \
+#define LOG_DEBUG(msg)                                                  \
+{                                                                       \
+    pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::DEBUG).log(msg);   \
 }
 
-#define LOG_INFO(msg)                                                \
-{                                                                    \
-    pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::INFO).log(msg); \
+#define LOG_INFO(msg)                                                   \
+{                                                                       \
+    pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::INFO).log(msg);    \
 }
 
 #define LOG_WARNING(msg)                                                \
@@ -61,14 +61,14 @@ namespace pqnet
     pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::WARNING).log(msg); \
 }
 
-#define LOG_ERROR(msg)                                                \
-{                                                                     \
-    pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::ERROR).log(msg); \
+#define LOG_ERROR(msg)                                                  \
+{                                                                       \
+    pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::ERROR).log(msg);   \
 }
 
-#define LOG_FATAL(msg)                                                \
-{                                                                     \
-    pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::FATAL).log(msg); \
+#define LOG_FATAL(msg)                                                  \
+{                                                                       \
+    pqnet::Logger(__FILE__, __LINE__, pqnet::Logger::FATAL).log(msg);   \
 }
 
 class Logger
