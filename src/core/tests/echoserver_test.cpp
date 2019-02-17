9c95615b50e172cb677f089f98dfb525013ee95c
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2019-02-18 00:28:33 +0800

    enrich echo

--- src/core/tests/echoserver_test.cpp
+++ src/core/tests/echoserver_test.cpp
@@ -1,8 +1,11 @@
+#include <functional>
 #include <iostream>
 
 #include "../../util/signal.h"
 #include "../callback.h"
 #include "../server.h"
+
+using namespace std::placeholders;
 
 void sighandler(int signo) {
     if (signo == SIGINT) {
@@ -11,22 +14,48 @@ void sighandler(int signo) {
     std::cout << "Exit echo server." << std::endl;
 }
 
-void onConnect(const pqnet::TcpConnPtr& conn) { conn->send("Hello!\n"); }
-void onClose(const pqnet::TcpConnPtr& conn) { conn->send("Bye!\n"); }
-void onRead(const pqnet::TcpConnPtr& conn) { conn->recv(); }
-void onMessage(const pqnet::TcpConnPtr& conn) { conn->send(); }
+class TcpEchoServer
+{
+public:
+    TcpEchoServer(std::uint16_t port) : serv(port) {}
+    void run() {
+        serv.setConnectCallBack(
+            std::bind(&TcpEchoServer::onConnect, this, _1)
+        );
+        serv.setCloseCallBack(
+            std::bind(&TcpEchoServer::onClose, this, _1)
+        );
+        serv.setReadCallBack(
+            std::bind(&TcpEchoServer::onRead, this, _1)
+        );
+        serv.setMessageCallBack(
+            std::bind(&TcpEchoServer::onMessage, this, _1)
+        );
+        serv.run();
+    }
+    void onConnect(const pqnet::TcpConnPtr& conn) {
+        conn->send("Hello!\n");
+    }
+    void onClose(const pqnet::TcpConnPtr& conn) {
+        conn->send("Bye!\n");
+    }
+    void onRead(const pqnet::TcpConnPtr& conn) {
+        conn->recv();
+    }
+    void onMessage(const pqnet::TcpConnPtr& conn) {
+        conn->send();
+    }
+private:
+    pqnet::TcpServer serv;
+};
 
 int main()
 {
-    pqnet::TcpServer serv(12488);
-    serv.setConnectCallBack(onConnect);
-    serv.setCloseCallBack(onClose);
-    serv.setReadCallBack(onRead);
-    serv.setMessageCallBack(onMessage);
+    TcpEchoServer echoserv(12488);
     pqnet::Signal sig(sighandler);
     sig.addSignal(SIGINT);
     sig.addSignal(SIGTERM);
     sig.waitSig();
-    serv.run();
+    echoserv.run();
     return 0;
 }
