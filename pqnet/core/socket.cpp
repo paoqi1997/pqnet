da28bfaabc73b46acce61889b28cb363813c84fd
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2020-02-10 00:57:04 +0800

    增加一些套接字选项相关函数

--- pqnet/core/socket.cpp
+++ pqnet/core/socket.cpp
@@ -1,4 +1,6 @@
 #include <fcntl.h>
+#include <netinet/in.h>  // for IPPROTO_TCP
+#include <netinet/tcp.h> // for TCP_NODELAY
 #include <sys/socket.h>
 
 #include "socket.h"
@@ -45,6 +47,26 @@ int pqnet::isReusePort(int sockfd)
     return opt;
 }
 
+int pqnet::isNoDelay(int sockfd)
+{
+    int opt;
+    socklen_t optlen = sizeof(opt);
+    if (getsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &opt, &optlen) == -1) {
+        return UNKNOWN;
+    }
+    return opt;
+}
+
+int pqnet::isKeepAlive(int sockfd)
+{
+    int opt;
+    socklen_t optlen = sizeof(opt);
+    if (getsockopt(sockfd, SOL_SOCKET, SO_KEEPALIVE, &opt, &optlen) == -1) {
+        return UNKNOWN;
+    }
+    return opt;
+}
+
 bool pqnet::setNonBlock(int sockfd, bool flag)
 {
     int flags = fcntl(sockfd, F_GETFL, 0);
@@ -77,6 +99,24 @@ bool pqnet::setReusePort(int sockfd, bool flag)
 {
     int opt = flag;
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) == -1) {
+        return false;
+    }
+    return true;
+}
+
+bool pqnet::setNoDelay(int sockfd, bool flag)
+{
+    int opt = flag;
+    if (setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt)) == -1) {
+        return false;
+    }
+    return true;
+}
+
+bool pqnet::setKeepAlive(int sockfd, bool flag)
+{
+    int opt = flag;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_KEEPALIVE, &opt, sizeof(opt)) == -1) {
         return false;
     }
     return true;
--- pqnet/core/socket.h
+++ pqnet/core/socket.h
@@ -5,12 +5,18 @@ namespace pqnet
 {
 
 int new_socket();
+
 int isNonBlock(int sockfd);
 int isReuseAddr(int sockfd);
 int isReusePort(int sockfd);
+int isNoDelay(int sockfd);
+int isKeepAlive(int sockfd);
+
 bool setNonBlock(int sockfd, bool flag);
 bool setReuseAddr(int sockfd, bool flag);
 bool setReusePort(int sockfd, bool flag);
+bool setNoDelay(int sockfd, bool flag);
+bool setKeepAlive(int sockfd, bool flag);
 
 } // namespace pqnet
 
